c89425fc571472d59efa02bb3d21256ec6845007
Angel Avendaño <angel.aven.c@gmail.com>
2014-11-02 20:17:27 -0600

    poner las imagenes en el admin del album

--- albums/admin.py
+++ albums/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Album
 
+class AlbumAdmin(admin.ModelAdmin):
+	list_display = ('title', 'imageAlbum', )
 
-admin.site.register(Album);
+	#obj es el elemneto de la lista que vamos a iterar
+	def imageAlbum(self, obj):
+		return ' <img src="%s" alt="" /> ' % obj.cover.url
+
+	imageAlbum.allow_tags = True;
+
+
+
+admin.site.register(Album, AlbumAdmin);
 
